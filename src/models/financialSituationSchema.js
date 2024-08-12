23443212c6d406dd1dcfb929ebc94b13790b973b
Gustavonobregab <gustavonobg@gmail.com>
2024-08-12 18:25:13 -0300

    schemasUpdate

--- src/models/financialSituationSchema.js
+++ src/models/financialSituationSchema.js
@@ -2,10 +2,36 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const financialSituationSchema = new Schema({
-  month: Number,
-  year: Number,
-  expenses: Number,
-  income: Number,
+  month: {
+    type: Number,
+    min: 1,
+    max: 12,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  income: {
+    type: Number,
+    default: 0,
+  },
+  expenses: {
+    type: Number,
+    default: 0,
+  },
+  balance: {
+    type: Number,
+    default: function () {
+      return this.income - this.expenses
+    },
+  },
+  transactions: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Transaction',
+    },
+  ],
 })
 
 const FinancialSituation = mongoose.model(
--- src/models/monthSchema.js
+++ src/models/monthSchema.js
@@ -1,11 +1,28 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const monthSchema = new Schema({
-  month: Number,
-  year: Number,
-  expenses: Boolean,
-  income: Boolean,
+const Transaction = new Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  type: {
+    type: [Debit, Credit, Cash],
+    default: [],
+  },
+  category: {
+    type: String,
+  },
+  description: {
+    type: mixed,
+  },
+  installments: {
+    type: [],
+  },
 })
 
 const Month = mongoose.model('Month', monthSchema)
