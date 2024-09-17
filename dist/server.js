54300e35b1d570eec336a52960f59fcdb2131373
Gustavonobg <gustavonobg@gmail.com>
2024-09-17 11:34:06 -0300

    ChangingToParse
    Starting to change to LLAmaParse instead of OLLama Local

--- dist/server.js
+++ dist/server.js
@@ -36,6 +36,32 @@ app.post('/financialsituation', async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+app.get('/financy/{id}'),
+    async (req, res) => {
+        try {
+            const id = req;
+            const productbyId = await FinancialSituation.find({ id });
+            console.log(req.body);
+            res.status(200).json(productbyId);
+        }
+        catch (error) {
+            console.error(error);
+            res.status(500).json({ message: error.message });
+        }
+    };
+app.put('/products/{id}'),
+    async (req, res) => {
+        try {
+            const id = req;
+            const productbyId = await FinancialSituation.find({ id });
+            console.log(req.body);
+            res.status(200).json(productbyId);
+        }
+        catch (error) {
+            console.error(error);
+            res.status(500).json({ message: error.message });
+        }
+    };
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -38,6 +38,32 @@ app.post('/financialsituation', async (req: Request, res: Response) => {
   }
 })
 
+app.get('/financy/{id}'),
+  async (req: Request, res: Response) => {
+    try {
+      const id = req
+      const productbyId = await FinancialSituation.find({ id })
+      console.log(req.body)
+      res.status(200).json(productbyId)
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ message: (error as Error).message })
+    }
+  }
+
+app.put('/products/{id}'),
+  async (req: Request, res: Response) => {
+    try {
+      const id = req
+      const productbyId = await FinancialSituation.find({ id })
+      console.log(req.body)
+      res.status(200).json(productbyId)
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ message: (error as Error).message })
+    }
+  }
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
